5d5092f369f2150a160edb1c83bb4f4eba471cfa
Samuel Carlsson <samuel.carlsson@volumental.com>
2017-09-14 13:24:01 +0200

    add/remove

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ window.loaded = function() {
     var ko = require('ko');
 
     function ViewModel() {
+        var thiz = this;
         this.firstName = "Foo";
         this.lastName = "Bar";
+        this.items = ko.observableArray([]);
+
+        this.remove = function(viewModel) {
+            var idx = thiz.items.indexOf(viewModel);
+            thiz.items.splice(idx, 1);
+        };
+
+        this.add = function() {
+            this.items.push({firstName: this.firstName, lastName: this.lastName, remove: this.remove});
+        };
     }
 
     ko.applyBindings(new ViewModel());
