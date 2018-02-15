b0b39d9f2166b45069df068ad9c8041afdd2a9eb
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-02-15 10:51:54 +0100

    wip - salvaging hd

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ var ko = require('knockout');
 
 function ViewModel() {
     var thiz = this;
-    this.firstName = "Foo";
-    this.lastName = "Bar";
+    this.firstName = ko.observable("Foo");
+    this.lastName = ko.observable("Bar");
     this.items = ko.observableArray([]);
+    this.search = ko.observable("");
+
+    this.fullName = ko.computed(function() { return thiz.firstName() + " " + thiz.lastName(); });
+
+    this.filtered = ko.computed(
+        function () {
+            return thiz.items().filter(
+                function(x) { return x.firstName.indexOf(thiz.search()) != -1; });
+            }
+    );
 
     this.remove = function(viewModel) {
         var idx = thiz.items.indexOf(viewModel);
@@ -12,17 +22,17 @@ function ViewModel() {
     };
 
     this.add = function() {
-        this.items.push({firstName: this.firstName, lastName: this.lastName, remove: this.remove});
+        this.items.push({firstName: this.firstName(), lastName: this.lastName(), remove: this.remove});
     };
 }
 
 window.loaded = function() {
-    /*ko.components.register('message-editor', {
+    ko.components.register('message-editor', {
         viewModel: function(params) {
             this.text = ko.observable(params && params.initialText || 'hi there');
         },
         template: require('html-loader!./components/message-editor.html')
-    });*/
+    });
     ko.applyBindings(new ViewModel());
 };
 
